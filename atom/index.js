505c86e646b10c522cfc003ca5e696fd6b1ef0ea
Russell Davis <551404+russelldavis@users.noreply.github.com>
2024-05-10 14:30:22 -0700

    WIP

--- atom/index.js
+++ atom/index.js
@@ -1,37 +1,43 @@
 import { clean } from '../clean-stores/index.js'
 
-let batchQueue = new Map()
+let batchQueue = []
+let batchOldValues = new Map()
 let batchChangedKeys = new Map()
 let batchLevel = 0
+let batchIndex = 0
 export let epoch = 0
 
 export let batch = (cb) => {
+  let queueWasEmpty = !batchQueue.length
   ++batchLevel
   try {
     return cb()
   } finally {
-    if (!--batchLevel && batchQueue.size) {
-      let oldQueue = batchQueue
-      let oldBatchChangedKeys = batchChangedKeys
-      batchQueue = new Map()
-      batchChangedKeys = new Map()
-      batch(() => {
-        // Iterating over keys instead of entries has better perf
-        for (let $atom of oldQueue.keys()) {
-          $atom._notify(oldQueue.get($atom), oldBatchChangedKeys.get($atom))
-        }
-      })
+    if (!--batchLevel && queueWasEmpty) {
+      runBatchQueue()
     }
   }
 }
+
+let runBatchQueue = () => {
+  for (batchIndex = 0; batchIndex < batchQueue.length; batchIndex++) {
+    batchQueue[batchIndex]._notify()
+  }
+  batchQueue.length = 0
+}
+
 
 export let atom = (initialValue) => {
   let listeners = new Set()
   let $atom = {
     _notify(oldValue, changedKey) {
+      let oldValue = batchOldValues.get($atom)
+      let changedKey = batchChangedKeys.get($atom)
       // Iterates over a copy so we don't get messed up by mutations during iteration
-      for (let listener of [...listeners]) {
-        if (listeners.has(listener)) listener($atom.get(), oldValue, changedKey)
+      for (let listener of listeners) {
+        // Left off here: what if this atom's value changes in the middle of this loop
+        // if (listeners.has(listener))
+        listener($atom.get(), oldValue, changedKey)
       }
     },
     get() {
